ba3edd0e272c01f008a8b7abaa4e57a6be1c47b5
Rathna-68980 <108972663+Rathna-68980@users.noreply.github.com>
2022-07-11 14:04:48 +0530

    Create DAY_1_prog_8.c

---
+++ DAY1/DAY_1_prog_8.c
@@ -0,0 +1,8 @@
+/***********8....Consider a character input and find its ASCII value. *************/
+#include <stdio.h>
+int main()
+{
+ //Logic for printing the ASCII value of a character.
+ char r='R';
+ printf("%d",r);
+}
