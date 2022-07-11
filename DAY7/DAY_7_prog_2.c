ac38047b74a01c8eb3875553048cee393beca68d
Rathna-68980 <108972663+Rathna-68980@users.noreply.github.com>
2022-07-11 17:03:20 +0530

    Create DAY_7_prog_2.c

---
+++ DAY7/DAY_7_prog_2.c
@@ -0,0 +1,20 @@
+/*******2.Write a code to swap even bits with odd bits in unsigned long variable(uint64).
+Example: bit 0 with bit 1; bit 2 with bit 3; bit 4 with bit*********/
+#include <stdio.h>
+//
+unsigned int swapBits(unsigned int x)
+{
+//Declaration of two variables.
+unsigned int even_bits = x & 0xAAAAAAAA;
+unsigned int odd_bits = x & 0x55555555;
+even_bits >>= 1;//Right shift all even bits.  
+odd_bits <<= 1;//Left shift all odd bits.
+return (even_bits | odd_bits);
+}
+int main()
+{
+    //assign the input number to variable x
+    unsigned int x = 64;
+    printf("%u ", swapBits(x));
+        return 0;
+}
